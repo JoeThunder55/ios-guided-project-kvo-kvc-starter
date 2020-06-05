41d6cc3d8d3ec77d5b050ebfe44ffc78092c19bd
Aaron Haughton <dreamcast2000@outlook.com>
2020-06-04 20:51:39 -0500

    Initial Commit

--- KVO-KVC/KVO KVC/ViewController.m
+++ KVO-KVC/KVO KVC/ViewController.m
@@ -26,7 +26,7 @@
 
     LSIDepartment *marketing = [[LSIDepartment alloc] init];
     marketing.name = @"Marketing";
-    LSIEmployee * philSchiller = [[LSIEmployee alloc] init];
+    LSIEmployee *philSchiller = [[LSIEmployee alloc] init];
     philSchiller.name = @"Phil";
     philSchiller.jobTitle = @"VP of Marketing";
     philSchiller.salary = 10000000; 
@@ -67,7 +67,14 @@
     
     NSLog(@"%@", self.hrController);
     
+    NSString *key = @"name";
+    NSString *value = [craig valueForKey:key];
+    NSLog(@"value for key %@", key, value);
     
+    for (id employee in engineering.employees) {
+        NSString *value = [employee valueForKey:key];
+        NSLog(@"value for key %@: %@", key, value);
+    }
 }
 
 
